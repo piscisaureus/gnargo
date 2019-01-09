ebf5f6ee903d2c1e5c9cc21775a23efbdf19c76b
Bert Belder <bertbelder@gmail.com>
2019-01-09 10:20:47 +0100

    Move tool

---
+++ move.js
@@ -0,0 +1,17 @@
+const { mkdirSync, renameSync } = require("fs");
+const { dirname, resolve, relative } = require("path");
+
+const dirs = require(`${__dirname}/work/package_dirs.json`);
+
+const cargo_home_src = `${__dirname}/work/cargo_home`;
+const cargo_home_dst = `${__dirname}/work/cargo_home2`;
+//const cargo_home_dst = `d:/deno3/third_party/rust_crates`;
+
+for (const src of dirs) {
+  const rel = relative(cargo_home_src, src);
+  const dst = resolve(cargo_home_dst, rel);
+  const dst_parent = dirname(dst);
+
+  mkdirSync(dst_parent, { recursive: true });
+  renameSync(src, dst);
+}
