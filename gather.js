187945edecdee5a578b011bfb51ca6c5d3e1c9d9
Bert Belder <bertbelder@gmail.com>
2019-02-01 12:36:48 -0800

    Remove stale code

--- gather.js
+++ gather.js
@@ -12,7 +12,6 @@ const dirs = require(`${__dirname}/work/package_dirs.json`);
 
 const cargo_home_src = `${__dirname}/work/cargo_home`;
 const cargo_home_dst = `${__dirname}/work/rust_crates`;
-//const cargo_home_dst = `d:/deno3/third_party/rust_crates`;
 
 for (const src of dirs) {
   const rel = relative(cargo_home_src, src);
