803e1c1137114f7efb854f950ae6ef7558aa6b8b
Bert Belder <bertbelder@gmail.com>
2019-03-31 17:49:22 -0400

    Work in d:/deno2

--- copygn.bat
+++ copygn.bat
@@ -1,3 +1,3 @@
-copy %~dp0work\BUILD.gn d:\deno\build_extra\rust
-move d:\deno\third_party\rust_crates %~dp0work\rust_crates_prev
-move %~dp0work\rust_crates d:\deno\third_party
+copy %~dp0work\BUILD.gn d:\deno2\build_extra\rust
+move d:\deno2\third_party\rust_crates %~dp0work\rust_crates_prev
+move %~dp0work\rust_crates d:\deno2\third_party
